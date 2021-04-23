7df2489f080a894d877f6c51de540645bebd64d9
rezavpn <83076350+rezavpn@users.noreply.github.com>
2021-04-23 21:44:56 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By RezaVpn        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By RezaVpn                           #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# HORASSS                                       #"
+echo -e "# Allah                          #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Admin Portal & White Vps                      #"
+echo -e "# @bebasberinternet                                 #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
